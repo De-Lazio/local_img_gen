2e3bb2b527955d0155c8ca7b2571389c539e1033
De-Lazio <84206924+De-Lazio@users.noreply.github.com>
2024-05-15 18:50:46 +0100

    Update local_to_image.py

--- local_to_image.py
+++ local_to_image.py
@@ -120,7 +120,50 @@ display(root)
 
 """
 
-import tkinter as tk
+import ipywidgets as widgets
+from IPython.display import display
+
+
+
+lg_fon = "Fon"
+lg_yor = "Yoruba"
+
+
+
+def translate_yor_en(phrase_yoruba):
+  encodage = tokenizer(phrase_yoruba, return_tensors="pt")  # conversion en tenseur PyTorch
+
+  #Predict translate
+  with torch.no_grad():
+    traduction = model.generate(**encodage, max_length=64, num_beams=2)  # on ajuste les paramètres
+
+  traduction_dechiffree = tokenizer.batch_decode(traduction, skip_special_tokens=True)[0]
+  return traduction_dechiffree
+
+def gen_img_yor_text(phrase_yoruba):
+  traduction_dechiffree = translate_yor_en(phrase_yoruba)
+  #Predict image
+  image = pipe(traduction_dechiffree).images[0]
+
+  image.save("new_img.png")
+  
+  # Convertir l'image PIL en données binaires
+  img_byte_arr = BytesIO()
+  image.save(img_byte_arr, format='PNG')
+  img_byte_arr = img_byte_arr.getvalue()
+
+  # Afficher l'image à partir des données binaires
+  img = Image.open(BytesIO(img_byte_arr))
+  img.show()
+
+  
+"""
+phrase_yoruba = "Aworan ti ile-iwe pẹlu awọn ọmọ ile-iwe"
+translate_sentent = translate_yor_en(phrase_yoruba)
+print(translate_sentent)
+
+gen_img_yor_text(phrase_yoruba)
+"""
 
 # Define font and colors
 LARGE_FONT = ("Verdana", 16)
@@ -132,81 +175,67 @@ BUTTON_HOVER_COLOR = "#0056b3"
 ENTRY_BORDER_COLOR = "#ccc"
 
 # Initialize the main window
-root = tk.Tk()
-
-# Define window dimensions and position
-window_width = 800
-window_height = 600
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x_pos = (screen_width - window_width) // 2
-y_pos = (screen_height - window_height) // 2
-
-root.title("Hackathon IA 2024")
-root.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
-root.resizable(width=False, height=False)
-root.config(background=BG_COLOR)
+root = widgets.Output()
 
 # Define header title
-header_label = tk.Label(root, text="HACKATHON IA 2024", font=LARGE_FONT, background=BG_COLOR, foreground=FONT_COLOR)
-header_label.grid(row=0, column=0, columnspan=2, pady=20)
+header_label = widgets.Label(value="HACKATHON IA 2024", layout=widgets.Layout(margin='20px 0px 0px 0px'), style={'description_width': 'initial'})
+display(header_label)
 
 # Define text input area
-text_entry = tk.Text(root, width=50, height=5, font=LARGE_FONT, borderwidth=5, relief="solid", bd=1, background="#fff")
-text_entry.grid(row=1, column=0, columnspan=2, pady=10)
+text_entry = widgets.Textarea(layout=widgets.Layout(width='50%', height='150px', margin='10px 0px 0px 0px'))
+display(text_entry)
 
 # Define translation buttons
-yoruba_to_french_button = tk.Button(
-    root, text="Yorouba en Français", font=BUTTON_FONT, background=BUTTON_COLOR, foreground=FONT_COLOR,
-    command=lambda: translate_text("yoruba"), activebackground=BUTTON_HOVER_COLOR
+yoruba_to_french_button = widgets.Button(
+    description="Yorouba en Français", layout=widgets.Layout(width='50%', margin='10px 0px 0px 0px'), button_style='info'
 )
-yoruba_to_french_button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+display(yoruba_to_french_button)
 
-fon_to_french_button = tk.Button(
-    root, text="Fon en Français", font=BUTTON_FONT, background=BUTTON_COLOR, foreground=FONT_COLOR,
-    command=lambda: translate_text("fon"), activebackground=BUTTON_HOVER_COLOR
+fon_to_french_button = widgets.Button(
+    description="Fon en Français", layout=widgets.Layout(width='50%', margin='10px 0px 0px 20px'), button_style='info'
 )
-fon_to_french_button.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
+display(fon_to_french_button)
 
 # Define image generation buttons
-yoruba_image_button = tk.Button(
-    root, text="Générer image par Yoruba", font=BUTTON_FONT, background=BUTTON_COLOR, foreground=FONT_COLOR,
-    command=lambda: generate_image("yoruba"), activebackground=BUTTON_HOVER_COLOR
+yoruba_image_button = widgets.Button(
+    description="Générer image par Yoruba", layout=widgets.Layout(width='50%', margin='10px 0px 0px 0px'), button_style='info'
 )
-yoruba_image_button.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
+display(yoruba_image_button)
 
-fon_image_button = tk.Button(
-    root, text="Générer image par Fon", font=BUTTON_FONT, background=BUTTON_COLOR, foreground=FONT_COLOR,
-    command=lambda: generate_image("fon"), activebackground=BUTTON_HOVER_COLOR
+fon_image_button = widgets.Button(
+    description="Générer image par Fon", layout=widgets.Layout(width='50%', margin='10px 0px 0px 20px'), button_style='info'
 )
-fon_image_button.grid(row=3, column=1, padx=10, pady=10, sticky="nsew")
+display(fon_image_button)
 
 # Define horizontal separator line
-separator_line = tk.Canvas(root, width=window_width, height=2, bg=BG_COLOR, highlightthickness=0)
-separator_line.create_line(0, 0, window_width, 0, fill=ENTRY_BORDER_COLOR)
-separator_line.grid(row=4, column=0, columnspan=2)
+separator_line = widgets.Output()
+with separator_line:
+    print('----------------------------------------------------------------------------------------')
+display(separator_line)
 
 # Define text result area
-text_result = tk.Label(root, width=50, height=10, font=LARGE_FONT, borderwidth=5, relief="solid", bd=1, background="#fff")
-text_result.grid(row=5, column=0, columnspan=2, padx= 75, pady=10, sticky="nsew")
-
-# Configure row and column to center content
-for i in range(6):
-    root.grid_rowconfigure(i, weight=1)
-
-for i in range(2):
-    root.grid_columnconfigure(i, weight=1)
+text_result = widgets.Label(value="", layout=widgets.Layout(width='50%', height='100px', margin='10px 0px 0px 0px'))
+display(text_result)
 
 def translate_text(language):
-    input_text = text_entry.get(1.0, tk.END)
+    input_text = text_entry.value
     # Implement translation logic based on the selected language (yoruba or fon)
-    translated_text = "Translation goes here"  # Placeholder for translated text
-    text_result.config(text=f"Traduction française: {translated_text}")
+    if language == lg_fon: 
+        translated_text = translate_yor_en(input_text)
+        text_result.value = f"Traduction Anglais: {translated_text}"
+    elif language == lg_yor:
+        translated_text = translate_yor_en(input_text)
+        text_result.value = f"Traduction Français: {translated_text}"
 
 def generate_image(language):
-    input_text = text_entry.get(1.0, tk.END)
+    input_text = text_entry.value
+    gen_img_yor_text(input_text)
     # Implement image generation logic based on the selected language (yoruba or fon)
     # Display the generated image or show a message box if image generation is successful or not
 
-root.mainloop()
+yoruba_to_french_button.on_click(lambda button: translate_text(lg_yor))
+fon_to_french_button.on_click(lambda button: translate_text(lg_fon))
+yoruba_image_button.on_click(lambda button: generate_image(lg_yor))
+fon_image_button.on_click(lambda button: generate_image(lg_fon))
 
+display(root)
